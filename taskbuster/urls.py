3cdfbaefc803c845ce6dad33bccdb430f6e253f9
Wen Bin Yu <wenbin@wens-mbp.attlocal.net>
2019-08-31 13:28:02 -0700

    Settings, static files and update'

--- taskbuster/urls.py
+++ taskbuster/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import url
 from django.urls import path
 
+from .views import home
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+	path('admin/', admin.site.urls),
+	url(r'^$', home, name='home'),
 ]
